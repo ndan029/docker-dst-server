ffeb4b9d492bfb610aec6565aca4293bb9fc9b6b
ndan029 <ndan029@gmail.com>
2022-07-06 20:08:20 -0700

    Update worldgenoverride.lua

--- dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
+++ dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -8,7 +8,6 @@ return {
         wildfires="rare",                      --     World | Wildfires                                                          never (None) rare (Little) default (Default) often (More) always (Tons) 
         carrots_regrowth="fast",               --     Resource Regrowth | Carrots                                                never (None) veryslow (Very Slow) slow (Slow) default (Default) fast (Fast) veryfast (Very Fast) 
         flowers_regrowth="fast",               --     Resource Regrowth | Flowers                                                never (None) veryslow (Very Slow) slow (Slow) default (Default) fast (Fast) veryfast (Very Fast) 
-        wasps="never",                          --     Hostile Creatures | Killer Bees                                            never (None) rare (Little) default (Default) often (More) always (Tons) 
 
 
                                                   -- World Generation Settings
