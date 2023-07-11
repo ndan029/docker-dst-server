6f38f4bf47e6e1e0c98a93afda8a45855eb232ba
Daniel Nguyen <daniel@local>
2023-07-11 00:44:21 -0700

    update settings

--- dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
+++ dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -2,19 +2,20 @@ return {
 	override_enabled = true,
 	preset = "SURVIVAL_TOGETHER", 			-- "SURVIVAL_TOGETHER", "MOD_MISSING", "SURVIVAL_TOGETHER_CLASSIC", "SURVIVAL_DEFAULT_PLUS", "COMPLETE_DARKNESS", "DST_CAVE", "DST_CAVE_PLUS"
 	overrides = {
-                                                  -- World Settings
-        frograin="rare",                       --     World | Frog Rain                                                          never (None) rare (Little) default (Default) often (More) always (Tons) 
-        hounds="rare",                         --     World | Hound Attacks                                                      never (None) rare (Little) default (Default) often (More) always (Tons) 
-        wildfires="rare",                      --     World | Wildfires                                                          never (None) rare (Little) default (Default) often (More) always (Tons) 
-        carrots_regrowth="fast",               --     Resource Regrowth | Carrots                                                never (None) veryslow (Very Slow) slow (Slow) default (Default) fast (Fast) veryfast (Very Fast) 
-        flowers_regrowth="fast",               --     Resource Regrowth | Flowers                                                never (None) veryslow (Very Slow) slow (Slow) default (Default) fast (Fast) veryfast (Very Fast) 
+		-- MISC
+		start_location = "plus", 	-- "caves", "default", "plus", "darkness"
+		world_size = "small", 		-- "small", "medium", "default", "huge"
+		branching = "most", 			-- "never", "least", "default", "most"
+		loop = "never", 				-- "never", "default", "always"
+		frograin = "rare", 			-- "never", "rare", "default", "often", "always"
+		wildfires = "never", 			-- "never", "rare", "default", "often", "always"
+		touchstone = "often", 		-- "never", "rare", "default", "often", "always"
+		boons = "often", 				-- "never", "rare", "default", "often", "always"
+		prefabswaps_start = "classic", 	-- "classic", "default", "highly random"
+		resettime="none"
 
+		-- ANIMALS
+		grassgekkos = "never"
 
-                                                  -- World Generation Settings
-        start_location="plus",                 --     World | Spawn Area                                                         plus (Plus) darkness (Dark) default (Default) 
-        branching="most",                      --     World | Branches                                                           never (Never) least (Least) default (Default) most (Most) random (Random) 
-        touchstone="often",                     --     World | Touch Stones                                                       never (None) rare (Little) uncommon (Less) default (Default) often (More) mostly (Lots) always (Tons) insane (Insane) 
-        boons="often",                          --     World | Failed Survivors                                                   never (None) rare (Little) uncommon (Less) default (Default) often (More) mostly (Lots) always (Tons) insane (Insane) 
-        mushroom="mostly",                       --     Resources | Mushrooms                                                      never (None) rare (Little) uncommon (Less) default (Default) often (More) mostly (Lots) always (Tons) insane (Insane) 
 	},
 }
